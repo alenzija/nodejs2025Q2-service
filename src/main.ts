c8c35551b4ef97c9f6e9f890f7a94b9fd000544a
Alena <alenzija@gmail.com>
2025-06-01 12:53:14 +0200

    fix: fix port

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { parse } from 'yaml';
-import { readFile } from 'node:fs/promises';
 import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'node:fs/promises';
+import { parse } from 'yaml';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const docConfig = await readFile('doc/api.yaml', 'utf8');
   const swaggerDocument = parse(docConfig);
   SwaggerModule.setup('/doc', app, swaggerDocument);
-  await app.listen(4000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
